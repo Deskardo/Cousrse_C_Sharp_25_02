ca4398e8999970d7649a2b1a25682dc583a00930
Deskardo <kalugin.aleksei.v@gmail.com>
2023-02-27 18:59:20 +0300

    правка в HW 2_0

--- HomeWork_2/2_0/Program.cs
+++ HomeWork_2/2_0/Program.cs
@@ -2,9 +2,9 @@
 
 int a = int.Parse(Console.ReadLine()!);
 
-while (a < 100)
+while (a < 100 || a > 1000)
 {
   Console.WriteLine("Число не является трёхзначным, введите трёхзначное число");
   a = int.Parse(Console.ReadLine()!);
 }
-Console.WriteLine((a % 100)/10);
+Console.WriteLine((a % 100) / 10);
---
+++ HomeWork_2/2_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int a = int.Parse(Console.ReadLine()!);
+
+if(a < 100)
+{
+  Console.WriteLine("Третей цифры нет");
+}
+
+Console.WriteLine(a);
+Console.WriteLine((a / 100 * 10) + (a % 10));
