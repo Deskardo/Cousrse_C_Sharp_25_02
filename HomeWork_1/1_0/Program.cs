a4f4df10fa12b255724197d4285e17bff8369f77
Deskardo <kalugin.aleksei.v@gmail.com>
2023-02-27 06:45:09 +0300

    HW 0_1

---
+++ HomeWork_1/1_0/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число наибольшее, а какое наименьшее
+
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+if (a > b)
+{
+  Console.WriteLine(a + "max" + " " + b + "min");
+}
+else 
+{
+  Console.WriteLine(b + "max" + " " + a + "min");
+}
