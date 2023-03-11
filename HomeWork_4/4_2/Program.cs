526dcbc32a904b19ff7e2eff56c85e08ed3e9448
Deskardo <kalugin.aleksei.v@gmail.com>
2023-03-11 08:30:31 +0300

    HW_04

---
+++ HomeWork_4/4_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов случайными числами
+// и выводит их на экран. Оформите заполнение массива и вывод в виде функции
+// (пригодится в следующих задачах)
+
+void Array(int lenght)
+{
+  int[] arr = new int [lenght];
+  for(int i = 0; i < lenght; i++)
+  {
+    arr[i] = new Random().Next(0, 100);
+    Console.Write(arr[i]);
+    Console.Write(", ");
+  }
+}
+
+Array(int.Parse(Console.ReadLine()!));
