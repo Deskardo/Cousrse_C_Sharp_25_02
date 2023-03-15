5d4572eaee3934f06241c2fffcbd2bad46963a82
Deskardo <kalugin.aleksei.v@gmail.com>
2023-03-15 07:07:16 +0300

    task 6_2

---
+++ lesson_6/6_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+string Convert(int num)
+{
+  string result = "";
+  while (num > 0)
+  {
+    result = num % 2 + result;
+    num = num / 2;
+  }
+  return result;
+}
+
+Console.WriteLine("Введите число для конвертации:");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Convert(a));
