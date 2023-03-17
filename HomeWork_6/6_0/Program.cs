674f6d7ad141347a7cf31bd1fed4db142de9b0da
Deskardo <kalugin.aleksei.v@gmail.com>
2023-03-17 20:47:35 +0300

    task 6_0

---
+++ HomeWork_6/6_0/Program.cs
@@ -0,0 +1,47 @@
+﻿// Напишите программу, которая будет создавать копию заданного массива с помощью
+// поэлементного копирования.
+
+void Print(double[] arr)
+{
+  int size = arr.Length;
+
+  for (int i = 0; i < size; i++)
+  {
+    Console.Write($"{arr[i]} ");
+  }
+  Console.WriteLine();
+}
+
+double[] MassNums(int size, double from, double to)
+{
+  double[] arr = new double[size];
+
+  for (int i = 0; i < size; i++)
+  {
+    arr[i] = to + new Random().NextDouble() * (from - to);
+  }
+  return arr;
+}
+
+double[] CopyArray(int size2, double[] firstArray)
+{
+  double[] finalArray = new double[size2];
+  for (int i = 0; i < size2; i++)
+  {
+    finalArray[i] = firstArray[i];
+  }
+  return finalArray;
+}
+
+
+Console.WriteLine("Введите размер массива");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите начало диапазона");
+double start = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите конец диапазона");
+double stop = int.Parse(Console.ReadLine()!);
+
+double[] mass = MassNums(num, start, stop);
+Print(mass);
+double[] copyMass = CopyArray(num, mass);
+Print(copyMass); 
