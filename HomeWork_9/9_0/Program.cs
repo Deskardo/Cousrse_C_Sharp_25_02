7acff50cd826ce443b264629384e5428a8437794
Deskardo <kalugin.aleksei.v@gmail.com>
2023-03-25 13:07:49 +0300

    HW 9_0

---
+++ HomeWork_9/9_0/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет
+// все четные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+Console.Clear();
+
+void EvenSum(int M, int N)
+{
+  if (M > N) return;
+  if (N % 2 == 1) N--;
+  EvenSum(M, N - 2);
+  Console.Write($"{N} ");
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+EvenSum(num, num2);
