72f09d6e14a500251c48f9f35ac3cac0ea8e4b79
Deskardo <kalugin.aleksei.v@gmail.com>
2023-03-25 10:29:35 +0300

    task 9_0

--- lesson_9/9_0/Program.cs
+++ lesson_9/9_0/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N при помои рекурсии.
+
+Console.Clear();
+
+void ValuesSet(int N)
+{
+  if (N == 0) return;
+  ValuesSet(N-1);
+  Console.Write($"{N} ");
+}
+
+int num = int.Parse(Console.ReadLine()!);
+ValuesSet(num);
